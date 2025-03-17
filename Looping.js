726ad923248f51ae0b213270eacb91a9a23500ff
Jesnajustin <maryjesnajustin@gmail.com>
2025-03-17 13:10:54 +0530

    welcome

---
+++ Looping.js
@@ -0,0 +1,25 @@
+// for of loop
+var array =[1,45,34,56,78]
+for (const i of array){
+    console.log(i);
+}
+for (let i of array){
+    console.log(i);
+}
+// while loop
+let i=0;
+while (i<10){
+    console.log(i+1);
+    i++
+}
+let j=0;
+while (j<10){
+    console.log(j);
+    j++
+}
+// do while loop
+let r=10;
+do{
+    console.log(r)
+    r++;
+} while(r<=15);
